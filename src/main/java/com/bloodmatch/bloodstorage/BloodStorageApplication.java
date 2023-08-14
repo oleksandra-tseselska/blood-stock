1786380796ab46d93007f3387c20b8b80c9c928a
Oleksandra Tseselska <oleksandra.tseselska@accenture.com>
2023-08-14 12:54:20 +0300

    init commit

---
+++ src/main/java/com/bloodmatch/bloodstorage/BloodStorageApplication.java
@@ -0,0 +1,13 @@
+package com.bloodmatch.bloodstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodStorageApplication.class, args);
+	}
+
+}
